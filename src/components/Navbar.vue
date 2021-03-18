51edbca17576ddb36e39017c58c11617f6fa297d
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2021-03-18 23:11:24 +0400

    add Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- <v-app> -->
+  <v-container fluid>
+    <v-app-bar color="deep-purple accent-4" dark>
+      <v-spacer />
+      <v-btn oulined class="ml-3">
+        <router-link to="/">Home</router-link>
+      </v-btn>
+      <v-btn oulined class="ml-3">
+        <router-link to="/photo">Photos</router-link>
+      </v-btn>
+    </v-app-bar>
+  </v-container>
+  <!-- </v-app> -->
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/pages/PhotosPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <Photo v-for="(photo, index) in photos" :key="index" />
+  </v-container>
+</template>
+
+<script>
+import Photo from "../components/photo/Photo";
+
+export default {
+  components: {
+    Photo,
+  },
+  data: () => ({
+    photos: [
+      { id: 1, title: "Photo 1" },
+      { id: 2, title: "Photo 2" },
+      { id: 3, title: "Photo 3" },
+      { id: 4, title: "Photo 4" },
+    ],
+  }),
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
